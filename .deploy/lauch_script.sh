3045df3d619b18c66d3a3c511b281748eef8e974
Marinarg <marinarg_09@hotmail.com>
2021-11-15 17:32:37 -0300

    update service

--- .deploy/lauch_script.sh
+++ .deploy/lauch_script.sh
@@ -1,1 +1,1 @@
-cd /home/ubuntu/search_eletronics_api && /home/ubuntu/.local/bin/uvicorn main:app --reload
+cd /home/admin/search_eletronics_api && /home/admin/miniconda3/envs/airflow/bin/uvicorn main:app --reload
